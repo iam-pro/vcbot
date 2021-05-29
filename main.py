863d240d1cbe10824be739b1c7a078281b8d1dd9
Caleb Noble <uniqisuniq@outlook.com>
2021-05-29 15:05:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def joinvc(_, m):
             except:
                 await bot.send_message(m.chat.id, "Already in Voice Chat!")
             return
-        await vc.join_group_call(m.chat.id, f"input.raw", bitrate=48000)
+        await vc.join_group_call(m.chat.id, f"input{m.chat.id}.raw", 48000, vc.get_cache_peer(), StreamType().local_stream,)
         await m.reply_text("Joined The Voice Chat!", quote=True)
 
     except Exception as e:
@@ -55,13 +55,17 @@ async def playvc(_, m):
     vc = vc.join_group_call(
         m.chat.id,
         "input{m.chat.id}.raw",
-        bitrate=48000
+        48000,
+        vc.get_cache_peer(),
+        StreamType().local_stream,
     )
     if not vc.is_connected:
         vc.join_group_call(
             m.chat.id,
             "input{m.chat.id}.raw",
-            bitrate=48000
+            48000,
+            vc.get_cache_peer(),
+            StreamType().local_stream,
         )
     msg = f"Playing {title} !"
     await m.reply(msg)
