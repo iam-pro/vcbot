3a2ee905becf2ded678b0b25e87dcd54fa50e239
Caleb Noble <uniqisuniq@outlook.com>
2021-05-29 17:46:26 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -4,6 +4,6 @@ ydl_opts = {"format": "bestaudio", "no-playlist": True}
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 
 async def get_yt_dict(query):
-  omk = ydl.extract_info(f"ytsearch:{query}", download=False)
-  yt = json.loads(omk)["entries"][0]
+  omk = ydl.extract_info(f"ytsearch:{query}", False)
+  yt = omk["entries"][0]
   return yt
