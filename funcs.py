0b9fde76f2c9419eaf6e0c07818242ccaf3bc71b
Caleb Noble <uniqisuniq@outlook.com>
2021-07-24 07:14:00 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,7 +1,36 @@
 import youtube_dl, json
+from client import QUEUE
 
 ydl_opts = {"format": "bestaudio", "no-playlist": True}
 ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+def add_to_queue(chat_id, song_name, from_user):
+    try:
+        n = sorted(list(QUEUE[int(chat_id)].keys()))
+        play_at = n[-1] + 1
+    except BaseException:
+        play_at = 1
+    if QUEUE.get(int(chat_id)):
+        QUEUE[int(chat_id)].update(
+            {
+                play_at: {
+                    "title": song_name,
+                    "from_user": from_user,
+                }
+            }
+        )
+    else:
+        QUEUE.update(
+            {
+                int(chat_id): {
+                    play_at: {
+                        "title": song_name,
+                        "from_user": from_user,
+                    }
+                }
+            }
+        )
+    return QUEUE[int(chat_id)]
 
 async def get_yt_dict(query):
   omk = ydl.extract_info(f"ytsearch:{query}", False)
