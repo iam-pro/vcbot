4e244278056dbe5da7a6584bcfc76ddaa19b2988
Caleb Noble <uniqisuniq@outlook.com>
2022-01-30 08:47:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import traceback, ffmpeg
 import asyncio
 from pytgcalls import PyTgCalls, StreamType, idle
+from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputAudioStream
 from pytgcalls.types.input_stream import InputStream
 from client import *
@@ -10,7 +11,22 @@ from funcs import *
 ydl_opts = {"format": "bestaudio"}
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 vc = PyTgCalls(user)
+#vc.start()
 queue = []
+
+def check_value(data, val):
+    if len(data) > 0:
+      for item in data:
+          if val in item.values():
+              print("sdasd Yes")
+              return item["is_playing"]
+          else:
+              print("sex")
+              return False
+    else:
+      print(len(data))
+      print("Nooooooooo")
+      return False
 
 def transcode(filename: str, chat_id: int):
     ffmpeg.input(filename).output(
@@ -33,7 +49,7 @@ def download(idd, chat_id):
 @bot.on_message(filters.command("vcs"))
 async def joinvc(_, m):
     try:
-        await m.reply_text(f"{vc.active_calls}", quote=True)
+        await m.reply_text(f"{vc.active_calls}\n\n{QUEUE}", quote=True)
 
     except Exception as e:
         print(traceback.print_exc())
@@ -41,16 +57,18 @@ async def joinvc(_, m):
 
 @bot.on_message(filters.command("skip"))
 async def skipvc(_, m):
-    vc.leave_group_call(m.chat.id)
+    await vc.leave_group_call(m.chat.id)
     await m.reply("skipped")
 
 @bot.on_message(filters.command("play"))
 async def playvc(_, m):
     text = m.text.split(" ", 1)
-#    if m.from_user.id not in AuthUsers:
-#        return
-    if vc.active_calls:
-        print(vc.active_calls)
+    print(AuthUsers)
+    if str(m.from_user.id) not in AuthUsers:
+        return
+    _check = check_value(json.loads(vc.active_calls.__str__()), m.chat.id)
+    print(_check)
+    if _check == False:
         ytdetails = await get_yt_dict(text[1])
         chat_id = m.chat.id
         info_dict = download(ytdetails["id"], chat_id)
@@ -70,27 +88,28 @@ async def playvc(_, m):
             stream_type=StreamType().local_stream
         )
         await m.reply(msg)
-    else:
+    elif _check == True:
+        print(m.chat.id, text[1], m.from_user.id)
         add_to_queue(m.chat.id, text[1], m.from_user.id)
         await m.reply("added to queue")
 
 @vc.on_stream_end()
-async def streamhandler(chat_id: int):
-    chat, song, from_user = get_from_queue(chat_id)
+async def streamhandler(vc: PyTgCalls, update: Update):
+    song, pos, from_user = get_from_queue(update.chat_id)
+    print(get_from_queue(update.chat_id))
     ytdetails = await get_yt_dict(song)
-    info_dict = download(ytdetails["id"], chat)
+    info_dict = download(ytdetails["id"], update.chat_id)
     title = info_dict["title"]
     thumb = info_dict["thumbnails"][1]["url"]
     duration = info_dict["duration"]
-    transcode(f"input{chat}.webm", chat)
+    transcode(f"input{update.chat_id}.webm", update.chat_id)
     msg = f"Playing {title} !"
-    await vc.change_stream(chat, InputStream(InputAudioStream(f"input{chat}.raw"),),)
-    QUEUE[chat_id].pop(pos)
-    msgg = await bot.send_message(f"Playing {song}")
-    if not QUEUE[chat]:
-        QUEUE.pop(chat)
+    await vc.change_stream(update.chat_id, InputStream(InputAudioStream(f"input{update.chat_id}.raw"),),)
+    QUEUE[update.chat_id].pop(pos)
+    msgg = await bot.send_message(update.chat_id, f"Playing {song}")
     await asyncio.sleep(duration + 5)
     await msgg.delete()
 
-p = Process(target=bot.run).start()
-vc.run()
+bot.start()
+vc.start()
+idle()
